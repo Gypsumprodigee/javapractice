27c8732e9d3f6b5c696157f1b0445ac327b5f58a
Gypsumprodigee <sanupurohit6@gmail.com>
2025-04-12 19:06:39 +0530

    sum_array.java
    Find Sum of Elements in Array

---
+++ sum_array.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+// Find Sum of Elements in Array
+public class sum_array 
+{
+    public static void main(String []args)
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter number of terms in the array :");
+       int t = sc.nextInt();
+       int arr []= new int [t];
+       int sum = 0;
+       System.out.println("Enter elements :");
+       for(int i = 0;i < t;i++)
+       {
+            arr [i]=sc.nextInt();
+            sum = sum + arr [i];
+       }
+       System.out.print("Sum of Elements : "+sum);
+    }
+}
