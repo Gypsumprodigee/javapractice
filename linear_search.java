1eae160bc2e1ebffa465297b90106026bbece8d3
Gypsumprodigee <sanupurohit6@gmail.com>
2025-04-16 00:12:46 +0530

    linear_search .java
    Performs linear search

---
+++ linear_search.java
@@ -0,0 +1,22 @@
+public class linear_search 
+{   
+    public static void main(String []args)
+    {
+        int [] a = {4,5,3,6,2,1,9,21};
+        int key = 20;
+        boolean flag = false;
+        for(int i = 0 ;i < a.length ; i++)
+        {
+            if (a[i] == key)
+            {
+                System.out.println(key+" is found at "+i+" position in array.");
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) 
+        {
+            System.out.println(key+" not found in the array.");
+        }
+    }
+}
