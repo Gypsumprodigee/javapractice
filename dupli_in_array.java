b7873c7af46b9be5f58e11939c07b5643e47b3e7
Gypsumprodigee <sanupurohit6@gmail.com>
2025-04-15 22:49:40 +0530

    dupli_in_array.java
    Find Duplicate Elements in Array

---
+++ dupli_in_array.java
@@ -0,0 +1,32 @@
+//Find Duplicate Elements in Array
+public class dupli_in_array 
+{
+    public static void main(String []args)
+    {
+        int arr[] = {1,1,3,5,4,3,1,7,6,6,6};
+        boolean visited[] = new boolean[arr.length-1];
+        int duplicate_count = 0;
+        for(int i = 0;i < arr.length;i++)
+        {
+            if(visited[i])
+            {
+                continue;
+            }
+            else
+            {
+                for(int j = i+1 ;j < arr.length;j++)
+                {
+                    if (arr[i] == arr[j]) 
+                    {
+                        visited[j] = true;
+                        duplicate_count++;
+                    }
+                }
+            }
+            if(duplicate_count > 0)
+            {
+                System.out.println(arr[i]+" has "+duplicate_count+" duplicate values in array.");
+            }
+        }
+    }
+}
